79748df9d4f090d938f865e8de796c64d2d48ec2
nerajno <nerajno@gmail.com>
2020-12-01 22:26:20 -0500

    making small chahges

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,21 +3,18 @@ import Conditions from '../Conditions/Conditions';
 
 const Forecast = () => {
 
-    let [ responseObj, setResponseObj ] = useState({}); 
+    const [ responseObj, setResponseObj ] = useState({}); // hooks
 
-    function getForecast() {
-      fetch(
-        "https://community-open-weather-map.p.rapidapi.com/weather?q=Atlanta&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html",
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-key":"d3b860f740mshc1d2a46e7966dfcp1e458ajsn4c6bcc397e7b",
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-          },
-        }
-      )
+    const getForecast = () => {
+            fetch("https://climacell-microweather-v1.p.rapidapi.com/weather/nowcast?lat=33.748997&lon=-84.387985",{
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "d3b860f740mshc1d2a46e7966dfcp1e458ajsn4c6bcc397e7b",
+                    "x-rapidapi-host": "climacell-microweather-v1.p.rapidapi.com"
+            }
+        })
         .then(response => response.json())
-        .then(response => {setResponseObj(response)
+        .then(response => {setResponseObj(response[0])
         })
         .catch((err) => {
           console.error(err);
@@ -30,7 +27,7 @@ const Forecast = () => {
             <div>
                 <h2>Find Current Weather Conditions</h2>
                 <div>
-                JSON.stringify(responseObj)
+                {JSON.stringify(responseObj)}
                 </div>
                 <button  onClick={getForecast}> Get Forecast</button>
                 <Conditions responseObj = {responseObj} />
